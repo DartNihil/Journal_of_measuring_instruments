0ef7ea47316f035548822d87ec68e3916ded355f
DartNihil <104585286+DartNihil@users.noreply.github.com>
2022-12-16 13:28:18 +0300

    Initial commit

---
+++ src/main/java/com/example/journal_of_measuring_instruments/JournalOfMeasuringInstrumentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.journal_of_measuring_instruments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalOfMeasuringInstrumentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalOfMeasuringInstrumentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/journal_of_measuring_instruments/JournalOfMeasuringInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.journal_of_measuring_instruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalOfMeasuringInstrumentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
